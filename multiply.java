626871eb9c0c2d35458c4c5564b18fd21720e2c0
Utsav Rohilla <utsavrohilla546@gmail.com>
2023-05-10 13:27:43 +0530

    Multiplication file added

---
+++ multiply.java
@@ -0,0 +1,14 @@
+class Utsav1{
+    public static void main(String[] args)
+    {
+        
+        int N = 7;
+ 
+        
+        for (int i = 1; i <= 10; i++) {
+
+            System.out.println(N + " * " + i + " = "
+                               + N * i);
+        }
+    }
+}
